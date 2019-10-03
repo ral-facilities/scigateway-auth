e216c24882fb4b6c1f1dfb99ce4828f4d4148afc
Keiran Price <keiran.price@stfc.ac.uk>
2019-10-03 09:29:16 +0100

    #10: Create authentication handler

--- auth.py
+++ auth.py
@@ -37,3 +37,19 @@ def get_authenticator(mnemonic):
         return LDAPICATAuthenticator()
 
 
+class AuthenticationHandler(object):
+    def __init__(self, mnemonic, credentials=None):
+        self.mnemonic = mnemonic
+        self.credentials = credentials if credentials is not None else None
+
+    def _get_payload(self):
+        authenticator = get_authenticator(self.mnemonic)
+        return authenticator.authenticate(self.credentials)
+
+    def _pack_JWT(self, dictionary):
+        # TODO
+        return dictionary
+
+    def get_jwt(self):
+        return self._pack_JWT(self._get_payload())
+
