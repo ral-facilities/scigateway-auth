b68bb9af08c6e4cab463fb00595c9c3e4c0ba2ff
Keiran Price <keiran.price@stfc.ac.uk>
2019-10-16 14:15:29 +0100

    #24: Add logging

--- scigateway-auth/src/auth.py
+++ scigateway-auth/src/auth.py
@@ -1,3 +1,4 @@
+import logging
 from functools import wraps
 
 import jwt
@@ -6,9 +7,12 @@ import requests
 from common.constants import SECRET, ICAT_AUTH_URL
 from common.exceptions import MissingMnemonicError
 
+log = logging.getLogger()
+
 
 class ICATAuthenticator(object):
     def authenticate(self, mnemonic, credentials=None):
+        log.info(f"Authenticating at {ICAT_AUTH_URL} with mnemonic: {mnemonic}")
         if credentials is None:
             return requests.post(ICAT_AUTH_URL,
                                  data={"json": f'{{"plugin": "{mnemonic}"}}'}).json()
@@ -22,6 +26,7 @@ class AuthenticationHandler(object):
     An AuthenticationHandler can be used to verify JWTs, insert sessions into JWTs and create ICATAuthenticators to
     get ICAT session IDs
     """
+
     def __init__(self):
         self.mnemonic = None
         self.credentials = None
@@ -37,6 +42,7 @@ class AuthenticationHandler(object):
         Creates an ICATAuthenticator and calls the authenticate method to get a payload
         :return: The payload
         """
+        log.info("Creating ICATAuthenticator")
         authenticator = ICATAuthenticator()
         return authenticator.authenticate(self.mnemonic, credentials=self.credentials)
 
@@ -46,7 +52,9 @@ class AuthenticationHandler(object):
         :param dictionary: the payload to be packed
         :return: The encoded JWT
         """
+        log.info("Encoding JWT")
         token = jwt.encode(dictionary, SECRET, algorithm="HS256")
+        log.info("Returning JWT")
         return token.decode("utf-8")
 
     def get_jwt(self):
@@ -63,9 +71,12 @@ class AuthenticationHandler(object):
         :return: - tuple with message and status code e.g. ("", 200)
         """
         try:
+            log.info("Verifying token")
             jwt.decode(token, SECRET, algorithms=["HS256"])
+            log.info("Token verified")
             return "", 200
         except:
+            log.warn("Token could not be verified")
             return "Unauthorized", 403
 
 
@@ -73,14 +84,16 @@ def requires_mnemonic(method):
     """
     Decorator for the /login post method to handle the case where a mnemonic is not provided
     """
+
     @wraps(method)
     def wrapper(*args, **kwargs):
         try:
             return method(*args, **kwargs)
-        except MissingMnemonicError:
+        except MissingMnemonicError as e:
+            log.exception(e)
             return "Missing mnemonic", 400
         except Exception as e:
-            print(e)
+            log.exception(e)
             return "Something went wrong", 500
 
     return wrapper
