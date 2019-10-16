894f6f948e720e8fdc0dda0f1f59deeb5fe4d7d7
Keiran Price <keiran.price@stfc.ac.uk>
2019-10-16 11:11:29 +0100

    #23: Add mock requests to ICAT authenticator

--- scigateway-auth/test/test_ICATAuthenticator.py
+++ scigateway-auth/test/test_ICATAuthenticator.py
@@ -1,7 +1,31 @@
-from unittest import TestCase
+from unittest import TestCase, mock
 
 from common.exceptions import BadMnemonicError
 from src.auth import ICATAuthenticator
+
+
+def mock_authenticated_icat_requests(*args, **kwargs):
+    class MockResponse(object):
+        def __init__(self, json_data, status_code):
+            self.json_data = json_data
+            self.status_code = status_code
+
+        def json(self):
+            return self.json_data
+
+    return MockResponse({"sessionId": "test"}, 200)
+
+
+def mock_unauthenticated_icat_request(*args, **kwargs):
+    class MockResponse(object):
+        def __init__(self, json_data, status_code):
+            self.json_data = json_data
+            self.status_code = status_code
+
+        def json(self):
+            return self.json_data
+
+    return MockResponse({"code": "SESSION", "message": "Error logging in. Please try again later"}, 200)
 
 
 class TestICATAuthenticator(TestCase):
