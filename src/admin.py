213bcbe837e61fbbcd48811e84052d62f8083f0d
Viktor Bozhinov <viktor.bozhinov@stfc.ac.uk>
2021-02-04 11:31:23 +0000

    Add logic to set states #50

--- src/admin.py
+++ src/admin.py
@@ -1,6 +1,5 @@
 import logging
 import json
-import jwt
 
 from common.constants import PUBLIC_KEY, BLACKLIST, MAINTENANCE_CONFIG_PATH, \
     SCHEDULED_MAINTENANCE_CONFIG_PATH
@@ -22,6 +21,24 @@ class MaintenanceMode(object):
         with open(MAINTENANCE_CONFIG_PATH, "r") as file:
             return json.load(file)
 
+    def set_state(self, state):
+        """
+        Updates the maintenance mode state in the maintenance.json file.
+        :param state: the state the JSON file to be updated with
+        :return: tuple with message and status code e.g. ("Maintenance state successfully
+                 updated", 200)
+        """
+        log.info("Attempting to update maintenance mode state")
+        try:
+            with open(MAINTENANCE_CONFIG_PATH, "w") as file:
+                json.dump(state, file)
+            log.info("Maintenance mode state successfully updated")
+            return "Maintenance mode state successfully updated", 200
+        except (FileNotFoundError, IOError, OverflowError, TypeError, ValueError):
+            log.warning("Failed to update maintenance mode state")
+            return "Failed to update maintenance mode state", 500
+
+
 class ScheduledMaintenanceMode(object):
     """
     Class that contains functions to get and set the scheduled maintenance mode state.
@@ -36,3 +53,20 @@ class ScheduledMaintenanceMode(object):
         log.info("Attempting to read scheduled maintenance mode state")
         with open(SCHEDULED_MAINTENANCE_CONFIG_PATH, "r") as file:
             return json.load(file)
+
+    def set_state(self, state):
+        """
+        Updates the scheduled maintenance mode state in the scheduled_maintenance.json file.
+        :param state: the state the JSON file to be updated with
+        :return: tuple with message and status code e.g. ("Scheduled maintenance mode state
+                 successfully updated", 200)
+        """
+        log.info("Attempting to update scheduled maintenance mode state")
+        try:
+            with open(SCHEDULED_MAINTENANCE_CONFIG_PATH, "w") as file:
+                json.dump(state, file)
+            log.info("Scheduled maintenance mode state successfully updated")
+            return "Scheduled maintenance mode state successfully updated", 200
+        except (FileNotFoundError, IOError, OverflowError, TypeError, ValueError):
+            log.warning("Failed to update scheduled maintenance mode state")
+            return "Failed to update scheduled maintenance mode state", 500
