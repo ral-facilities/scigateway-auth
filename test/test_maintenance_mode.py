c51f0b9ede34f6173b869ce9167d03dfe7641ece
Viktor Bozhinov <viktor.bozhinov@stfc.ac.uk>
2021-02-04 11:39:19 +0000

    Unit test setting states #50

--- test/test_maintenance_mode.py
+++ test/test_maintenance_mode.py
@@ -1,6 +1,7 @@
 import json
 from unittest import TestCase, mock
 
+from src import admin
 from src.admin import MaintenanceMode
 
 with open("test/keys/jwt-key.pub", "r") as f:
@@ -18,3 +19,19 @@ class TestMaintenanceMode(TestCase):
     @mock.patch("builtins.open", mock.mock_open(read_data=json.dumps(MAINTENANCE_STATE)))
     def test_get_state(self):
         assert self.maintenance_mode.get_state() == MAINTENANCE_STATE
+
+    def test_set_state(self):
+        mock_json_dump = mock.patch.object(admin.json, "dump").start()
+        with mock.patch("builtins.open", mock.mock_open()) as mocked_file:
+            result = self.maintenance_mode.set_state(MAINTENANCE_STATE)
+
+        mocked_file.assert_called_once_with(MAINTENANCE_CONFIG_PATH, 'w')
+        mock_json_dump.assert_called_once_with(MAINTENANCE_STATE,
+                                               mocked_file.return_value)
+        self.assertEqual(result, ("Maintenance mode state successfully updated", 200))
+
+    def test_set_state_file_update_failure(self):
+        with mock.patch("builtins.open", mock.mock_open()) as mocked_file:
+            mocked_file.side_effect = IOError()
+            result = self.maintenance_mode.set_state(MAINTENANCE_STATE)
+        self.assertEqual(result, ("Failed to update maintenance mode state", 500))
--- test/test_scheduled_maintenance_mode.py
+++ test/test_scheduled_maintenance_mode.py
@@ -1,6 +1,7 @@
 import json
 from unittest import TestCase, mock
 
+from src import admin
 from src.admin import ScheduledMaintenanceMode
 
 with open("test/keys/jwt-key.pub", "r") as f:
@@ -19,3 +20,19 @@ class TestScheduledMaintenanceMode(TestCase):
     @mock.patch("builtins.open", mock.mock_open(read_data=json.dumps(SCHEDULED_MAINTENANCE_STATE)))
     def test_get_state(self):
         assert self.scheduled_maintenance_mode.get_state() == SCHEDULED_MAINTENANCE_STATE
+
+    def test_set_state_valid_token(self):
+        mock_json_dump = mock.patch.object(admin.json, "dump").start()
+        with mock.patch("builtins.open", mock.mock_open()) as mocked_file:
+            result = self.scheduled_maintenance_mode.set_state(SCHEDULED_MAINTENANCE_STATE)
+
+        mocked_file.assert_called_once_with(SCHEDULED_MAINTENANCE_CONFIG_PATH, 'w')
+        mock_json_dump.assert_called_once_with(SCHEDULED_MAINTENANCE_STATE,
+                                               mocked_file.return_value)
+        self.assertEqual(result, ("Scheduled maintenance mode state successfully updated", 200))
+
+    def test_set_state_file_update_failure(self):
+        with mock.patch("builtins.open", mock.mock_open()) as mocked_file:
+            mocked_file.side_effect = IOError()
+            result = self.scheduled_maintenance_mode.set_state(SCHEDULED_MAINTENANCE_STATE)
+        self.assertEqual(result, ("Failed to update scheduled maintenance mode state", 500))
