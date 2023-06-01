f21707de76925fc9b24d012cf8e99f44924c5ed0
Viktor Bozhinov <45173816+VKTB@users.noreply.github.com>
2023-06-01 17:40:36 +0000

    Ignore werkzeug vulnerabilities

--- noxfile.py
+++ noxfile.py
@@ -75,6 +75,8 @@ def safety(session):
         # Ignore 51457 as the vulnerability has not yet being fixed
         # Ignore 52322 and 52518 as the latest version of Gitpython does not
         # support python 3.6 which is still used in production
+        # Ignore 53325, 53326, and 54456 as the fixed versions do not support
+        # python 3.6
         session.run(
             "safety",
             "check",
@@ -86,6 +88,12 @@ def safety(session):
             "52322",
             "--ignore",
             "52518",
+            "--ignore",
+            "53325",
+            "--ignore",
+            "53326",
+            "--ignore",
+            "54456",
         )
 
         try:
