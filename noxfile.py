6db30b2a5be353da1d7168fe3e62f0c8515fe1c3
VKTB <45173816+VKTB@users.noreply.github.com>
2024-11-04 16:34:41 +0000

    Update noxfile.py #133

--- noxfile.py
+++ noxfile.py
@@ -8,112 +8,40 @@ nox.options.sessions = "lint", "safety", "tests"
 code_locations = "scigateway_auth", "test", "noxfile.py"
 
 
-def install_with_constraints(session, *args, **kwargs):
-    # Auto file deletion is turned off to prevent a PermissionError experienced on
-    # Windows
-    with tempfile.NamedTemporaryFile(delete=False) as requirements:
-        session.run(
-            "poetry",
-            "export",
-            "--dev",
-            "--format=requirements.txt",
-            "--without-hashes",
-            f"--output={requirements.name}",
-            external=True,
-        )
-        session.install(f"--constraint={requirements.name}", *args, **kwargs)
-
-        try:
-            # Due to delete=False, the file must be deleted manually
-            requirements.close()
-            os.unlink(requirements.name)
-        except IOError:
-            session.log("Error: The temporary requirements file could not be closed")
-
-
 @nox.session(reuse_venv=True)
 def black(session):
     args = session.posargs or code_locations
-
-    install_with_constraints(session, "black")
+    session.run("poetry", "install", "--only=dev", external=True)
     session.run("black", *args, external=True)
 
 
 @nox.session(reuse_venv=True)
 def lint(session):
     args = session.posargs or code_locations
-    install_with_constraints(
-        session,
-        "flake8",
-        "flake8-bandit",
-        "flake8-black",
-        "flake8-broken-line",
-        "flake8-bugbear",
-        "flake8-builtins",
-        "flake8-commas",
-        "flake8-comprehensions",
-        "flake8-import-order",
-        "flake8-logging-format",
-        "pep8-naming",
-    )
+    session.run("poetry", "install", "--only=dev", external=True)
     session.run("flake8", *args)
 
 
 @nox.session(reuse_venv=True)
 def safety(session):
-    install_with_constraints(session, "safety")
-    with tempfile.NamedTemporaryFile(delete=False) as requirements:
-        session.run(
-            "poetry",
-            "export",
-            "--dev",
-            "--format=requirements.txt",
-            "--without-hashes",
-            f"--output={requirements.name}",
-            external=True,
-        )
-        # Ignore 51457 as the vulnerability has not yet being fixed
-        # Ignore 52322 and 52518 as the latest version of Gitpython does not
-        # support python 3.6 which is still used in production
-        # Ignore 53325, 53326, 54456, and 55261 as the fixed versions do not support
-        # python 3.6
-        session.run(
-            "safety",
-            "check",
-            f"--file={requirements.name}",
-            "--full-report",
-            "--ignore",
-            "51457",
-            "--ignore",
-            "52322",
-            "--ignore",
-            "52518",
-            "--ignore",
-            "53325",
-            "--ignore",
-            "53326",
-            "--ignore",
-            "54456",
-            "--ignore",
-            "55261",
-            "--ignore",
-            "58755",
-            "--ignore",
-            "59062",
-            "--ignore",
-            "59473",
-        )
-
-        try:
-            # Due to delete=False, the file must be deleted manually
-            requirements.close()
-            os.unlink(requirements.name)
-        except IOError:
-            session.log("Error: The temporary requirements file could not be closed")
+    session.install("safety")
+    # Ignore 70612, 72731 and 70624 as they have not yet been fixed
+    session.run(
+        "safety",
+        "check",
+        "--file=poetry.lock",
+        "--full-report",
+        "--ignore",
+        "70612",
+        "--ignore",
+        "72731",
+        "--ignore",
+        "70624",
+    )
 
 
-@nox.session(python=["3.6", "3.7", "3.8", "3.9"], reuse_venv=True)
+@nox.session(python=["3.11"], reuse_venv=True)
 def tests(session):
     args = session.posargs
-    session.run("poetry", "install", external=True)
+    session.run("poetry", "install", "--with=dev", external=True)
     session.run("pytest", *args)
